a6055f95e6b98c654cd2542c69c5527c015502ff
Tunde <tundaey@gmail.com>
2018-01-10 07:10:32 +0000

    fixed container api

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 docker-compose run api knex migrate:latest --env development 
 docker-compose run api knex seed:run --env development
-
-docker-compose run api knex migrate:latest --env test 
-docker-compose run api knex seed:run --env test
